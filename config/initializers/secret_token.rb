bdbb37867f9ab91051ada07e7defaf36271fdf68
Rikin Diwan <rikin.diwan@gmail.com>
2013-07-09 19:43:51 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jcdb::Application.config.secret_token = '131bb511abd5db1b39794d76f7abece61e20aa858e4b02214963a62e2050c5821a7c2599e010e9e0a6ee7e1a09ea777e9c4f9b88c0888aa64032dc5dd6ae1991'
